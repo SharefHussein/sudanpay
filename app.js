c3359b2f7f8ac0e3b5741ac5b541db8f78f870ad
SharefHussein <sharefhessien8@gmail.com>
2025-12-19 14:11:54 +0200

    Update app.js

--- app.js
+++ app.js
@@ -1,15 +1,1 @@
-// Firebase config
-var firebaseConfig = {
-  apiKey: "AIzaSyB3vxJu_et-P80ek30I3MRdC_lGhooCCsc",
-  authDomain: "sudanpay-e332a.firebaseapp.com",
-  projectId: "sudanpay-e332a",
-  storageBucket: "sudanpay-e332a.appspot.com",
-  messagingSenderId: "699809447272",
-  appId: "1:699809447272:web:90f3780ed6c768c4322add"
-};
 
-// Initialize Firebase
-firebase.initializeApp(firebaseConfig);
-
-const auth = firebase.auth();
-const db = firebase.firestore();
