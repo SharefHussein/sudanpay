3209b7fa03badac1d57063d4c89b06a26f28c30c
SharefHussein <sharefhessien8@gmail.com>
2025-12-19 12:08:43 +0200

    Create firebase.js

---
+++ firebase.js
@@ -0,0 +1,17 @@
+<script src="https://www.gstatic.com/firebasejs/8.10.0/firebase-app.js"></script>
+<script src="https://www.gstatic.com/firebasejs/8.10.0/firebase-auth.js"></script>
+<script src="https://www.gstatic.com/firebasejs/8.10.0/firebase-database.js"></script>
+
+<script>
+  var firebaseConfig = {
+    apiKey: "AIzaSyB3vxJu_et-P80ek30I3MRdC_lGhooCCsc",
+    authDomain: "sudanpay-e332a.firebaseapp.com",
+    projectId: "sudanpay-e332a",
+    databaseURL: "https://sudanpay-e332a-default-rtdb.firebaseio.com",
+    storageBucket: "sudanpay-e332a.appspot.com",
+    messagingSenderId: "699809447272",
+    appId: "1:699809447272:web:90f3780ed6c768c4322add"
+  };
+
+  firebase.initializeApp(firebaseConfig);
+</script>
