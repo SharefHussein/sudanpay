19e31996671cab60f387d979bf4e35f7fc569301
SharefHussein <sharefhessien8@gmail.com>
2025-12-19 16:08:07 +0200

    Update auth.js

--- auth.js
+++ auth.js
@@ -1,49 +1,28 @@
-import { 
-  getAuth,
-  createUserWithEmailAndPassword,
-  signInWithEmailAndPassword,
-  onAuthStateChanged,
-  signOut
-} from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
+import { auth } from "./firebase.js";
+import { createUserWithEmailAndPassword, signInWithEmailAndPassword, onAuthStateChanged, signOut } from "https://www.gstatic.com/firebasejs/9.23.0/firebase-auth.js";
 
-import { app } from "./firebase.js";
-
-const auth = getAuth(app);
-
-// تسجيل حساب
 window.register = function () {
   const email = document.getElementById("email").value;
   const password = document.getElementById("password").value;
-
   createUserWithEmailAndPassword(auth, email, password)
-    .then(() => {
-      window.location.href = "dashboard.html";
-    })
+    .then(() => location.href = "dashboard.html")
     .catch(err => alert(err.message));
 };
 
-// تسجيل دخول
 window.login = function () {
   const email = document.getElementById("email").value;
   const password = document.getElementById("password").value;
-
   signInWithEmailAndPassword(auth, email, password)
-    .then(() => {
-      window.location.href = "dashboard.html";
-    })
+    .then(() => location.href = "dashboard.html")
     .catch(err => alert(err.message));
 };
 
-// حماية الصفحات
-onAuthStateChanged(auth, (user) => {
-  if (!user && location.pathname.includes("dashboard")) {
-    window.location.href = "login.html";
-  }
-});
-
-// تسجيل خروج
 window.logout = function () {
-  signOut(auth).then(() => {
-    window.location.href = "login.html";
-  });
+  signOut(auth).then(() => location.href = "login.html");
 };
+
+onAuthStateChanged(auth, user => {
+  if (!user && location.pathname.includes("dashboard")) {
+    location.href = "login.html";
+  }
+}); 
