cb021a1efe5bbe53b6ae21f40581dd23f6ba4ec0
SharefHussein <sharefhessien8@gmail.com>
2025-12-19 15:09:34 +0200

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,1 +1,8 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
 
+export const app = initializeApp({
+  apiKey: "ضع_apiKey_هنا",
+  authDomain: "PROJECT_ID.firebaseapp.com",
+  projectId: "PROJECT_ID",
+  appId: "APP_ID"
+});
