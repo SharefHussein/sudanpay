c36bb341d5bebc28f2e84124bc73495c16704361
SharefHussein <sharefhessien8@gmail.com>
2025-12-20 09:59:01 +0200

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,15 +1,17 @@
-// Firebase configuration and initialization
-import { initializeApp } from "https://www.gstatic.com/firebasejs/9.23.0/firebase-app.js";
-import { getAuth } from "https://www.gstatic.com/firebasejs/9.23.0/firebase-auth.js";
+<!-- firebase.js -->
+<script type="module">
+  import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.4/firebase-app.js";
+  import { getAuth } from "https://www.gstatic.com/firebasejs/10.12.4/firebase-auth.js";
 
-const firebaseConfig = {
-  apiKey: "AIzaSyB3vxJu_et-P80ek30I3MRdC_lGhooCCsc",
-  authDomain: "sudanpay-e332a.firebaseapp.com",
-  projectId: "sudanpay-e332a",
-  storageBucket: "sudanpay-e332a.appspot.com",
-  messagingSenderId: "699809447272",
-  appId: "1:699809447272:web:90f3780ed6c768c4322add"
-};
+  const firebaseConfig = {
+    apiKey: "AIzaSyB3vxJu_et-P80ek30I3MRdC_lGhooCCsc",
+    authDomain: "sudanpay-e332a.firebaseapp.com",
+    projectId: "sudanpay-e332a",
+    storageBucket: "sudanpay-e332a.appspot.com",
+    messagingSenderId: "699809447272",
+    appId: "1:699809447272:web:90f3780ed6c768c4322add"
+  };
 
-const app = initializeApp(firebaseConfig);
-export const auth = getAuth(app);
+  window.firebaseApp = initializeApp(firebaseConfig);
+  window.auth = getAuth(firebaseApp);
+</script>
