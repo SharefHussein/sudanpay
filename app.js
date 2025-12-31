12dfc499c51bd040376d7eadb89cdc779e95f305
SharefHussein <sharefhessien8@gmail.com>
2025-12-31 10:17:03 +0200

    Update app.js

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
-// تهيئة Firebase (الكونفيج بتاعك)
+// إعدادات Firebase - سيتم استبدال القيم تلقائياً بواسطة GitHub Actions
 const firebaseConfig = {
-  apiKey: "AIzaSyB3vxJu_et-P80ek30I3MRdC_lGhooCCsc",
-  authDomain: "sudanpay-e332a.firebaseapp.com",
-  projectId: "sudanpay-e332a",
-  storageBucket: "sudanpay-e332a.firebasestorage.app",
+  apiKey: "REPLACE_WITH_API_KEY",
+  authDomain: "REPLACE_WITH_AUTH_DOMAIN",
+  projectId: "REPLACE_WITH_PROJECT_ID",
+  storageBucket: "sudanpay-e332a.appspot.com",
   messagingSenderId: "699809447272",
   appId: "1:699809447272:web:90f3780ed6c768c4322add"
 };
