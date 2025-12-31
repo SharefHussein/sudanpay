8a9a7be87d10bd66e8abe1ef8976fd506e9ca208
SharefHussein <sharefhessien8@gmail.com>
2025-12-31 10:19:26 +0200

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -3,10 +3,11 @@ import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/fireba
 import { getAuth } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
 import { getFirestore } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
 
+// إعدادات Firebase - سيتم استبدال القيم تلقائياً بواسطة GitHub Actions
 const firebaseConfig = {
-  apiKey: "AIzaSyB3vxJu_et-P80ek30I3MRdC_lGhooCCsc",
-  authDomain: "sudanpay-e332a.firebaseapp.com",
-  projectId: "sudanpay-e332a",
+  apiKey: "REPLACE_WITH_API_KEY",
+  authDomain: "REPLACE_WITH_AUTH_DOMAIN",
+  projectId: "REPLACE_WITH_PROJECT_ID",
   storageBucket: "sudanpay-e332a.appspot.com",
   messagingSenderId: "699809447272",
   appId: "1:699809447272:web:90f3780ed6c768c4322add"
