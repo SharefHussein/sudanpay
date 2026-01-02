647542f8d7635b081c005d4b54e45f483a63e1a9
SharefHussein <sharefhessien8@gmail.com>
2026-01-02 13:17:45 +0200

    Update app.js

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
+// app.js
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
-import { getAuth, GoogleAuthProvider, signInWithPopup, createUserWithEmailAndPassword, signInWithEmailAndPassword, sendPasswordResetEmail, onAuthStateChanged, signOut } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
-import { getFirestore, doc, setDoc, getDoc, collection, addDoc, serverTimestamp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
+import { getAuth, GoogleAuthProvider } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
 
-// إعداداتك
 const firebaseConfig = {
   apiKey: "AIzaSyB3vxJu_et-P80ek30I3MRdC_lGhooCCsc",
   authDomain: "sudanpay-e332a.firebaseapp.com",
@@ -13,10 +13,7 @@ const firebaseConfig = {
   measurementId: "G-XRN6CBLKXY"
 };
 
-const app = initializeApp(firebaseConfig);
-const auth = getAuth(app);
-const db = getFirestore(app);
-const provider = new GoogleAuthProvider();
-
-export { auth, db, provider, setDoc, doc, getDoc, collection, addDoc, serverTimestamp, signInWithPopup, createUserWithEmailAndPassword, signInWithEmailAndPassword, sendPasswordResetEmail, onAuthStateChanged, signOut };
-
+export const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);
+export const db = getFirestore(app);
+export const provider = new GoogleAuthProvider();
